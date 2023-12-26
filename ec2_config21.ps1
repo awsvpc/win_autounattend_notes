78b169234763f5ccdc2c41eea08ef6b5d9fac8e5
awsvpc <88736238+awsvpc@users.noreply.github.com>
2023-12-25 19:07:58 -0500

    Create ec2_config21.ps1

---
+++ ec2_config21.ps1
@@ -0,0 +1,24 @@
+# https://github.com/mefellows/windows-machine-factory-tutorial/blob/master/machine-factory/scripts/Ec2Config.ps1
+write-output "Updating Ec2 Config..."
+$EC2SettingsFile="C:\\Program Files\\Amazon\\Ec2ConfigService\\Settings\\Config.xml"
+$xml = [xml](get-content $EC2SettingsFile)
+$xmlElement = $xml.get_DocumentElement()
+$xmlElementToModify = $xmlElement.Plugins
+
+foreach ($element in $xmlElementToModify.Plugin)
+{
+    if ($element.name -eq "Ec2SetPassword")
+    {
+        $element.State="Enabled"
+    }
+    elseif ($element.name -eq "Ec2SetComputerName")
+    {
+        $element.State="Enabled"
+    }
+    elseif ($element.name -eq "Ec2HandleUserData")
+    {
+        $element.State="Enabled"
+    }
+}
+$xml.Save($EC2SettingsFile)
+write-output "Updated Ec2 Config."
